e26d0897929b6c6e65ba7237aac086ae0edea501
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-06-16 19:47:29 +0100

    react basics

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,25 @@
 const Rating = ()=> {
+
+    const clicked = (index) => console.log(`star ${index} was clicked`)
+    
+    const hovered = (direction, index) => console.log('Hovered', direction, index)
     return (
 
         <div className="rating-container">
             <h2>Rate Your Experience</h2>
-
+            <div className="stars">
+                {[1,2,3,4,5].map((star, index)=>(
+                    <span 
+                        onClick={()=> clicked(index)}
+                        onMouseEnter={()=> hovered('enter', index)}
+                        onMouseLeave={()=> hovered('leave', index)}
+                        key={ star }
+                        className="star"
+                    >
+                        { '\u2605' }
+                    </span>
+                ))}
+            </div>
         </div>
     )
 }
