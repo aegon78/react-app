231970a5e0f4c58dfc1910c9e29cdd26e1761f7b
aegon78 <204925927+aegon78@users.noreply.github.com>
2026-06-20 09:02:01 +0100

    rc

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import ListGroup from './components/ListGroup'
 
 
 function App() {
-  return <div> <ListGroup /> </div>
-  
+  let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']
 
+  return <div> <ListGroup items={items} heading='cities' /> </div>
 }
 
 export default App
--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -1,17 +1,32 @@
-function ListGroup() {
-    return (
-        <>
-            <h2>Hello</h2>
-            <ul className="list-group">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
-            </ul>
-        </>
-    
-);
+import { useState } from 'react';
+
+
+function ListGroup({items, heading}) {
+
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+
+  return (
+    <>
+      <h2>{heading}</h2>
+      {items.length === 0 && 'items not found'}
+
+      <ul className="list-group">
+        {items.map((item, idx) => (
+          <li
+            className={
+              selectedIndex === idx
+                ? 'list-group-item active'
+                : 'list-group-item'
+            }
+            key={item}
+            onClick={() => (setSelectedIndex(idx))}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
